2c43a83b683cecb186ff70f8ea11ef9aa6d1528f
zintaolin12 <zintaolin@gmail.com>
2025-12-22 18:37:41 +0000

    work

--- vega-app/vite.config.ts
+++ vega-app/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, 'src/app'),
+      components: path.resolve(__dirname, 'src/components'),
+      features: path.resolve(__dirname, 'src/features'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      lib: path.resolve(__dirname, 'src/lib'),
+    },
+  },
 })
