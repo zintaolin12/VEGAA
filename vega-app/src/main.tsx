72824aedada1bae256be9471b05f3265de08dd71
zintaolin12 <zintaolin@gmail.com>
2025-12-23 19:47:56 +0000

    workss

--- vega-app/src/main.tsx
+++ vega-app/src/main.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import AppLayout from './app/layout'
-import Providers from './app/providers'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Providers>
-      <BrowserRouter>
-        <AppLayout />
-      </BrowserRouter>
-    </Providers>
+    <div style={{ color: 'blue', padding: 40, fontSize: 24 }}>
+      ROOT IS RENDERING
+    </div>
   </React.StrictMode>
 )
