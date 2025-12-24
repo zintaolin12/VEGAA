2cd77202003564c8bc69c4c27dd5a1623bbf9699
zintaolin12 <zintaolin@gmail.com>
2025-12-24 20:59:45 +0000

    workingo

--- vega-app/src/App.tsx
+++ vega-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./features/dashboard/Dashboard";
 import MarketsPage from "./features/markets/MarketsPage";
 import EarnPage from "./features/earn/EarnPage";
@@ -9,22 +9,20 @@ import MobileNav from "./components/layout/MobileNav";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-[#020617] text-white pb-16">
-        <Topbar />
+    <div className="min-h-screen bg-[#020617] text-white pb-16">
+      <Topbar />
 
-        <main className="px-3 pt-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/markets" element={<MarketsPage />} />
-            <Route path="/earn" element={<EarnPage />} />
-            <Route path="/swap" element={<SwapPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-          </Routes>
-        </main>
+      <main className="px-3 pt-4">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/markets" element={<MarketsPage />} />
+          <Route path="/earn" element={<EarnPage />} />
+          <Route path="/swap" element={<SwapPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </main>
 
-        <MobileNav />
-      </div>
-    </BrowserRouter>
+      <MobileNav />
+    </div>
   );
 }
