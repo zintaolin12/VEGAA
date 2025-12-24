d2d7890bd99a568fdd7f2df447ea6e50b752896e
zintaolin12 <zintaolin@gmail.com>
2025-12-24 21:49:18 +0000

    workingo

--- vega-app/src/main.tsx
+++ vega-app/src/main.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import { WagmiProvider, createConfig, http } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { metaMask, walletConnect } from "wagmi/connectors";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App";
 import "./index.css";
 
+/* ===========================
+   React Query (REQUIRED)
+=========================== */
+const queryClient = new QueryClient();
+
+/* ===========================
+   Wagmi config
+=========================== */
 const config = createConfig({
   chains: [mainnet],
   connectors: [
@@ -20,12 +32,17 @@ const config = createConfig({
   },
 });
 
+/* ===========================
+   App bootstrap
+=========================== */
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WagmiProvider config={config}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </WagmiProvider>
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </WagmiProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
